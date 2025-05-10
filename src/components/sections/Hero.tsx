0ded8e8bc1fa8bad2884f01ff7f362e98114321d
Dakota Swank <60488787+d-swank@users.noreply.github.com>
2025-05-09 18:59:14 -0700

    Fixed minor spacing issues. Fixed scroll bar dark/light toggle but still need to fix scrollbar delay

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
   return (
     <section
       id="hero"
-      className="min-h-screen flex flex-col items-center justify-center text-center bg-gradient-to-r from-white via-slate-200 to-slate-300 dark:from-gray-600 dark:via-gray-800 dark:to-gray-900 text-gray-900 dark:text-white text-4xl scroll-mt-20"
+      className="min-h-screen flex flex-col items-center justify-center text-center bg-gradient-to-r from-white via-slate-200 to-slate-300 dark:from-gray-600 dark:via-gray-800 dark:to-gray-900 text-gray-900 dark:text-white text-4xl scroll-mt-20 transition-colors"
     >
       <motion.h1
         className="text-5xl md:text-7xl font-bold font-mono mb-4"
@@ -20,13 +20,13 @@ export default function Hero() {
         </span>
       </motion.h1>
       <motion.p
-        className="text-xl md:text-2xl font-sans max-w-xl mx-auto mb-6 leading-relaxed"
+        className="text-xl md:text-2xl font-sans max-w-3xl mb-6 px-5 md:px-5 leading-relaxed"
         initial={{ opacity: 0, x: 500 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 3 }}
       >
-        I’m a San Diego-based Software QA Engineer who is passionate about
-        creating beautiful and functional websites for fun!
+        I’m a San Diego-based Software QA Engineer who is driven by detail and
+        passionate about performance.
       </motion.p>
     </section>
   );
